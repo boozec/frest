f3348678b43dc407001032a011ba29b2e5a29c31
Santo Cariotti <dcariotti24@gmail.com>
2020-03-25 21:49:10 +0100

    feat: init manage

---
+++ src/frest/manage.py
@@ -0,0 +1,12 @@
+import argparse
+from manage.utils import *
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    logo()
+    main()
