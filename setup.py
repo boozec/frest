9773dbc24d103867934a1678ed90bb57957adccb
Santo Cariotti <dcariotti24@gmail.com>
2020-04-06 21:55:15 +0200

    feat: init setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="frest",
+    version="0.0.17",
+    author="Santo Cariotti",
+    description="Write REST API quickly",
+    url="https://github.com/dcariotti/frest",
+    packages=setuptools.find_packages(),
+    package_data={"frest": ["templates/*.txt"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    scripts=["bin/frest"],
+)
