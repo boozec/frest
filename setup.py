15c76a66ec5289d0fd3c5aa2c1537ad0c1a715c4
Santo Cariotti <dcariotti24@gmail.com>
2020-04-10 11:33:21 +0200

    release: 0.1.0

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+def requirements():
+    lines = []
+    with open("requirements.txt") as f:
+        for line in f.readlines():
+            line = line.strip()
+            lines.append(line)
+
+    return sorted(list(set(lines)))
+
+
 setuptools.setup(
     name="frest",
-    version="0.0.19",
+    version="0.1.0",
     author="Santo Cariotti",
     description="Write REST API quickly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/dcariotti/frest",
     packages=setuptools.find_packages(),
+    install_requires=requirements(),
     package_data={"frest": ["templates/*.txt"]},
     classifiers=[
         "Programming Language :: Python :: 3",
