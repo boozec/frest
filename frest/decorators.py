051d022004d91cea75630cccbea43a2b652b5f4e
Santo Cariotti <dcariotti24@gmail.com>
2020-06-29 09:34:02 +0200

    fix: remove useless code

--- frest/decorators.py
+++ frest/decorators.py
@@ -11,7 +11,7 @@ def check_token(f):
         if not headers.get("Authorization"):
             abort(403)
 
-        auth = request.headers.get("Authorization")
+        auth = headers.get("Authorization")
         token = Token.query.filter_by(string=auth).first()
         if not token:
             abort(403)
@@ -28,8 +28,6 @@ def check_token(f):
 def admin_required(f):
     @wraps(f)
     def inner(*args, **kwargs):
-        header = request.headers
-
         auth = request.headers.get("Authorization")
         token = Token.query.filter_by(string=auth).first()
         if not token.user.is_admin:
